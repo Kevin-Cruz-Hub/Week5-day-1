bb88d396c42b9e4566c53ec57b07b3c104656f9d
Kevin <kevincruz042@gmail.com>
2023-07-13 15:28:50 -0400

    all completed

--- homework.js
+++ homework.js
@@ -1,32 +1,32 @@
-// class Cat{
-//     constructor(name, color, age, weight){
-//         this.name = name;
-//         this.color = color;
-//         this.age = age;
-//         this.weight = weight;
-//     }
-//     isHungry(){
-//         this.isHungry = true;
-//         console.log(`Please feed ${this.name}.`)
-//     }
-//     isFull(){
-//         this.isFull = true;
-//         console.log(`${this.name} ate, Come back later.`)
-//     }
-//     toString(){
-//         console.log(`${this.name} is a ${this.color} cat`)
-//     }
+class Cat{
+    constructor(name, color, age, weight){
+        this.name = name;
+        this.color = color;
+        this.age = age;
+        this.weight = weight;
+    }
+    isHungry(){
+        this.isHungry = true;
+        console.log(`Please feed ${this.name}.`)
+    }
+    isFull(){
+        this.isFull = true;
+        console.log(`${this.name} ate, Come back later.`)
+    }
+    toString(){
+        console.log(`${this.name} is a ${this.color} cat`)
+    }
 
-// }
-// const Frisky = new Cat('Frisky','Black and grey', 13, 20);
-// const Garfield = new Cat('Garfield','orange',30,50 ) 
-// Garfield.isHungry()
-// Garfield.toString()
+}
+const Frisky = new Cat('Frisky','Black and grey', 13, 20);
+const Garfield = new Cat('Garfield','orange',30,50 ) 
+Garfield.isHungry()
+Garfield.toString()
 
-// Frisky.isFull()
-// Frisky.toString()
+Frisky.isFull()
+Frisky.toString()
 
-// ===================================================
+console.log('===================================================')
 class Pirate {
 
     constructor(crewmate, height, clean, ability){
@@ -34,6 +34,18 @@ class Pirate {
         this.height = height;
         this.clean = clean  
         this.ability = ability
+    }
+    statement(){
+        this.statement = statement;
+        console.log(`My name is ${this.Captain}, and I will be the Pirate King`)
+    }
+    vow(){
+        this.statement = statement;
+        console.log(`${this.Captain} is the strongest man alive`);
+    }
+    declare(){
+        this.statement = statement;
+        console.log(`${this.captain} is the freest man on the sea`)
     }
   }
 const luffy = new Pirate('Luffy',5.7,false, 'rubber-man')
@@ -51,8 +63,8 @@ MobyDick.push(Whitebeard, Marco, Ace)
 
 
 for(let pirates of thousandSunny){
-    console.log(pirates.crewmate, pirates.height, pirates.clean, pirates.ability)
+    console.log(`${pirates.crewmate}: ${pirates.height}, ${pirates.clean}, ${pirates.ability}`)
 }
 for(let pirates of MobyDick){
-    console.log(pirates.crewmate, pirates.height, pirates.clean, pirates.ability)
+    console.log(`${pirates.crewmate}: ${pirates.height}, ${pirates.clean}, ${pirates.ability}`)
 }
